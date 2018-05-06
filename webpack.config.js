44da6cc4c3bf9be61da190d8a25cfbb7b38b7edf
Ak-lee <laklc@126.com>
2018-05-06 17:08:44 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV==='development'
 const config= {
@@ -21,27 +22,6 @@ const config= {
                 loader: 'babel-loader'
             },
             {
-                test:/\.css$/,
-                use:[
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test:/\.styl$/,
-                use:[
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader:'postcss-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    'stylus-loader'
-                ]
-            },
-            {
                 test:/\.(gif|jpg|jpeg|png|svg)$/,
                 use:[{
                     loader: "url-loader",
@@ -50,7 +30,8 @@ const config= {
                         name:'[name]-aaa.[ext]'   // 使用原来本身的名字。ext 为文件后缀名
                     }
                 }]
-            }
+            },
+
         ]
     },
     plugins: [
@@ -64,6 +45,20 @@ const config= {
 }
 
 if(isDev){
+    config.module.rules.push({
+        test:/\.styl$/,
+        use:[
+            'style-loader',
+            'css-loader',
+            {
+                loader:'postcss-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            'stylus-loader'
+        ]
+    })
     config.devtool = '#cheap-module-eval-source-map'
     config.devServer={
         port:'8888',
@@ -80,6 +75,37 @@ if(isDev){
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
+    )
+}else{
+    config.entry = {
+        app: path.join(__dirname, 'src/index.js'),
+        vendor: ["vue"]
+    }
+    config.output.filename = '[name].[chunkhash:8].js'
+    config.module.rules.push({
+        test:/\.styl$/,
+        use:ExtractPlugin.extract({
+            fallback: 'style-loader',
+            use:[
+                'css-loader',
+                {
+                    loader:'postcss-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                'stylus-loader'
+            ]
+        })
+    })
+    config.plugins.push(
+        new ExtractPlugin('styles.[contentHash:8].css'),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'runtime'
+        })
     )
 }
 module.exports=config
